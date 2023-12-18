28576df6da20ae8a223546fdaba7ac1b938c8c6f
Motivationman_74 <120492095+Motivetionman@users.noreply.github.com>
2023-12-19 02:47:12 +0700

    Update lab9_1.cpp

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,33 +1,49 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip>
+
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main() {
+    double loan;
+    double rate_per_year;
+    double amount_you_can_pay_per_year;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
+    cout << "Enter initial loan: ";
+    cin >> loan;
+    cout << "Enter interest rate per year (%): ";
+    cin >> rate_per_year;
+    cout << "Enter amount you can pay per year: ";
+    cin >> amount_you_can_pay_per_year;
+
+    cout << setw(13) << left << "EndOfYear#";
+    cout << setw(13) << left << "PrevBalance";
+    cout << setw(13) << left << "Interest";
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
+
+    cout << fixed << setprecision(2);
+
+    int year = 1;
+    while (loan > 0) {
+        double interest = loan * (rate_per_year / 100);
+        double total = loan + interest;
+        double payment = min(total, amount_you_can_pay_per_year);
+        double new_balance = total - payment;
+
+        cout << setw(13) << left << year;
+        cout << setw(13) << left << loan;
+        cout << setw(13) << left << interest;
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << payment;
+        cout << setw(13) << left << new_balance;
+        cout << "\n";
+
+        loan = new_balance;
+        year++;
+    }
+
+    return 0;
 }
+	
